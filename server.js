26f6162ee1e375b135ae9949a68b0b017e4a3f40
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-03-25 21:49:15 +0530

    server.js

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require('express');
 
 const app = express();
 
+const ejs = require('ejs')
+const path = require('path')
+
+const expressLayout = require('express-ejs-layouts')
+
 const PORT = process.env.PORT || 3000;  // ES6 syntax for if-else.
 
+app.get('/' , (req, res) =>{
+    // res.send('Server Started')
+    res.render('home')
+    
+})
+
+// template Engine 
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine' , 'ejs')
+
+
 app.listen(PORT, () => {
-    console.log('listening on port 3000')
+    console.log(`listening on port ${PORT}...`)
 })
