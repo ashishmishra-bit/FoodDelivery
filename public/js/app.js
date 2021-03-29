a44e34620be4c60714c361a3e6be6d076b570dfa
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-03-29 12:45:41 +0530

    cart

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,10 @@
   \*****************************/
 /***/ (() => {
 
-
+// window.addEventListener("scroll", function(){
+//     var nav = document.querySelector("nav");
+//     nav.classList.toggle("sticky" , window.scrollY > 0);
+// })
 
 /***/ }),
 
