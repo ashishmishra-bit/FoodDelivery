0d283d8a984cf9974ea0c025e1792376f5a75f8f
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-04-02 09:09:50 +0530

    all controller fixed

--- routes/web.js
+++ routes/web.js
@@ -1,20 +1,18 @@
 const homeController = require('../app/http/controllers/homeController')
+const authController = require('../app/http/controllers/authController')
 
 function initRoutes (app){
 
     homeController().index
     app.get('/' ,  homeController().index) 
-    
+
     app.get('/cart' , (req, res) =>{
         res.render('coustomers/cart')
     })
-    app.get('/login' , (req, res) =>{
-        res.render('auth/login')
-    })
+    app.get('/login', authController().login)
     
-    app.get('/register' , (req, res) =>{
-        res.render('auth/register')
-    })
+    app.get('/register', authController().register)
+        
 }
 
 module.exports = initRoutes;
