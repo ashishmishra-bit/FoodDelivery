0ca213f304b2a911f98231ac0495cfeb02a11e95
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-04-02 08:37:17 +0530

    home controller

--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,10 @@
-function initRoutes (app){
-    app.get('/' , (req, res) =>{        
-        res.render('home')
-        
-    })
+const homeController = require('../app/http/controllers/homeController')
 
+function initRoutes (app){
+
+    homeController().index
+    app.get('/' ,  homeController().index) 
+    
     app.get('/cart' , (req, res) =>{
         res.render('coustomers/cart')
     })
